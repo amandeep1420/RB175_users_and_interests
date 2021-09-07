16963b3b7075f975c81fd3c87cc1379f98a057b6
amandeep1420 <amandeep1420@gmail.com>
2021-09-07 21:24:09 +0000

    Syntax cleanup.

--- users_and_interests.rb
+++ users_and_interests.rb
@@ -1,12 +1,12 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
-require "yaml"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+require 'yaml'
 
 helpers do
   def count_interests
     interest_count = 0
-    @database.each do |user, info_hash|
+    @database.each do |_, info_hash|
       interest_count += info_hash[:interests].size
     end
     interest_count
@@ -14,30 +14,29 @@ helpers do
 end
 
 before do
-  @database       = YAML.load_file("users.yaml")
+  @database       = YAML.load_file('users.yaml')
   @users          = @database.keys
   @user_count     = @users.size
   @interest_count = count_interests
 end
 
 not_found do
-  redirect "/"
+  redirect '/'
 end
 
-get "/" do
-  redirect "/user_list"
+get '/' do
+  redirect '/user_list'
 end
 
-get "/user_list" do
+get '/user_list' do
   erb :user_list
 end
 
-get "/users/:user" do
+get '/users/:user' do
   @user      = params[:user].to_sym
   @class     = @user.class
   @email     = @database[@user][:email]
   @interests = @database[@user][:interests].join(', ')
-  
+
   erb :user_profile_page
 end
-  
